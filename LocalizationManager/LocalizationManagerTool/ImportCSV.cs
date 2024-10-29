a6f485df4a743a24835b49a1ec4dc5a6feef947e
ABlinOffi <arthur.blin.officiel@gmail.com>
2024-10-29 16:48:30 +0100

    ImportCSV

---
+++ LocalizationManager/LocalizationManagerTool/ImportCSV.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LocalizationManagerTool
+{
+    public partial class MainWindow
+    {
+        private void OpenCSV(object sender, RoutedEventArgs e)
+        {
+            // Configure open file dialog box
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.FileName = "Document"; // Default file name
+            dialog.DefaultExt = ".csv"; // Default file extension
+            dialog.Filter = "Excel Documents (.csv)|*.csv"; // Filter files by extension
+
+            // Show open file dialog box
+            bool? result = dialog.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                string filename = dialog.FileName;
+            }
+        }
+    }
+}
